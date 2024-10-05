66fa29b543b4be2a361c424d2c2f3056c79cbf45
HelenMenasie <helenmenassie94@gmail.com>
2024-10-05 07:25:29 +0300

    update doc

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/3333x8
 export default defineConfig({
   plugins: [react()],
   base: "/Netflix-Clone-2024/",
